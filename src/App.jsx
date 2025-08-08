5e34c552a83e80b347fbede9c617b13c1816b364
junaidanwar365 <junaidanwar365@gmail.com>
2025-08-08 16:30:11 +0500

    fix: correct manager component import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
-import Manager from './components/manager'
+import Manager from './components/Manager'
 import Footer from './components/Footer'
 function App() {
   
